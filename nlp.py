178567eade559bdb37f27bdab08fb1e4eef373b6
Gil Wiraatmadja <gilwiraatmadja@gmail.com>
2017-11-25 03:02:21 +0100

    NLP test
    Tokenize, Stopword removal, POS Tagging

--- nlp.py
+++ nlp.py
@@ -1,11 +1,26 @@
 import nltk
+from nltk.corpus import stopwords
 
-texts="Hello Mr. Smith, how are you doing today? The weather is great, and Python is awesome. The sky is pinkish-blue. You shouldn't eat cardboard."
+texts="Hello Mr. Trump, how are you doing today? The weather is great, and Python is awesome. The sky is pinkish-blue. You shouldn't eat cardboard."
 
-sentences = nltk.sent_tokenize(texts)
-for sentence in sentences:
-	words = nltk.word_tokenize(sentence)
-	tagged_words = nltk.pos_tag(words)
-	ne_tagged_words = nltk.ne_chunk(tagged_words)
-	print ne_tagged_words
+
+#Tokenize
+words = nltk.word_tokenize(texts)
+
+#Stopwords Removal
+stopset = set(stopwords.words('english'))
+words = [w for w in words if not w in stopset]
+
+sno = nltk.stem.SnowballStemmer('english')
+
+for word in words:
+	#Stemming
+	stem_word = sno.stem(word)
+	print stem_word
 	
+#POS-TAG
+tagged_words = nltk.pos_tag(words)
+
+#named entities
+ne_tagged_words = nltk.ne_chunk(tagged_words)
+print ne_tagged_words
