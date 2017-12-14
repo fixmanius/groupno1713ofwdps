341912ae9568a1306fa24a69b61ef3bae280ffa1
Gil Wiraatmadja <gilwiraatmadja@gmail.com>
2017-12-14 21:27:24 +0100

    update

--- readme.md
+++ readme.md
@@ -2,6 +2,8 @@ Running example:
 
 run_wdps1713.sh WARC-Record-ID hdfs:///user/bbkruit/CommonCrawl-sample.warc.gz
 
+This runs on ELASTICSEARCH and TRIDENT at node093
+
 1. The script opens the HDFS file based on the path provided in the argument (argv 2) and collects the content as text.
 2. The text (HDFS file content) is then split with "WARC/1.0" as the delimiter, this results in a list of text where each element roughly represents one warc file.
 3. Iterate through each element, and only further process ones that are of WARC type response.
@@ -9,5 +11,6 @@ run_wdps1713.sh WARC-Record-ID hdfs:///user/bbkruit/CommonCrawl-sample.warc.gz
 5. Use beautiful soup to extract only text content in <body>.
 6. Go through entity_recognition function which performs NLP. The output of the function is a list of proper nouns (NNP).
 7. For each noun in step 6, go to do_query function which runs the query of the noun in elastic search.
-8. Out of the candidates from elastic search, query each in trident and then return entity ID of the one with most popular one (freebase score * SPARQL hit).
-9. Go through each noun and stream the results (key, noun, entity ID) to output.
+8. Filter the candidates from the elastic search by comparing the label against the noun using hamming distance (<=2), characater dice coefficient (>0.9), and bigram dice coefficient (0.6).
+9. Out of the filtered candidates, query each in trident and then return entity ID of the one with most popular one (freebase score * SPARQL hit).
+9. Stream the result (key, noun, entity ID) to output.
--- src/Assignment 1/warc_nlp_pyspark.py
+++ src/Assignment 1/warc_nlp_pyspark.py
@@ -65,7 +65,38 @@ def entity_recognition(text):
 		'''
 	
 	return results_list
+	
+def hamming_dist(str1, str2):
+	diffs = 0
+	for ch1, ch2 in zip(str1, str2):
+		if ch1 != ch2:
+			diffs += 1
+	return diffs
 
+def char_dice_coefficient(a, b):
+    a_bigrams = set(a)
+    b_bigrams = set(b)
+    overlap = len(a_bigrams & b_bigrams)
+    return overlap * 2.0/(len(a_bigrams) + len(b_bigrams))
+
+def bigram_dice_coefficient(a, b):
+    """dice coefficient 2nt/na + nb."""
+    if not len(a) or not len(b): return 0.0
+    if len(a) == 1:  a=a+u'.'
+    if len(b) == 1:  b=b+u'.'
+    
+    a_bigram_list=[]
+    for i in range(len(a)-1):
+      a_bigram_list.append(a[i:i+2])
+    b_bigram_list=[]
+    for i in range(len(b)-1):
+      b_bigram_list.append(b[i:i+2])
+      
+    a_bigrams = set(a_bigram_list)
+    b_bigrams = set(b_bigram_list)
+    overlap = len(a_bigrams & b_bigrams)
+    dice_coeff = overlap * 2.0/(len(a_bigrams) + len(b_bigrams))
+    return dice_coeff
 
 def do_query(query):
 	import requests
@@ -85,13 +116,14 @@ def do_query(query):
 	if response:
 		response = response.json()
 		for hit in response.get('hits', {}).get('hits', []):
-			freebase_id = hit.get('_source', {}).get('resource')
 			label = hit.get('_source', {}).get('label')
-			score = hit.get('_score', 0)
+			if hamming_dist(query,label)<=2 and bigram_dice_coefficient(query,label)>0.6 and char_dice_coefficient(query,label)>0.9:
+				freebase_id = hit.get('_source', {}).get('resource')
+				score = hit.get('_score', 0)
 
-			ids.add( freebase_id )
-			scores[freebase_id] = max(scores.get(freebase_id, 0), score)
-			labels.setdefault(freebase_id, set()).add( label )
+				ids.add( freebase_id )
+				scores[freebase_id] = max(scores.get(freebase_id, 0), score)
+				labels.setdefault(freebase_id, set()).add( label )
 
 	#print('Found %s results.' % len(labels))
 
@@ -126,7 +158,11 @@ def do_query(query):
 			facts[i] = n
 
 	def get_best(i):
-		return math.log(facts[i]) * scores[i]
+		ret=0
+		if facts[i]!=0:
+			#print math.log(facts[i]),"*",scores[i]
+			ret=math.log(facts[i]) * scores[i]
+		return ret
 
 	#print('Best matches:')
 	best=None
